d67f0daa539b8380dab0ae92e8e59bc363988ea3
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:11:23 +0300

    Created ChooseLessonProgramRequest Dto.

---
+++ src/main/java/sehmus/school_management_system/payload/requests/concretes/ChooseLessonProgramRequest.java
@@ -0,0 +1,22 @@
+package sehmus.school_management_system.payload.requests.concretes;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChooseLessonProgramRequest {
+
+    @NotNull(message = "Please select lesson program")
+    @Size(min=1, message = "lessons must not be empty")
+    private Set<Long> lessonProgramId;
+
+}
