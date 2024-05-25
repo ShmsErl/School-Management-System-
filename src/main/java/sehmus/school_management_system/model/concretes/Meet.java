ed74c9f6a734f7672d118675e369522e0812f425
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 12:14:31 +0300

    Created Meet class.

---
+++ src/main/java/sehmus/school_management_system/model/concretes/Meet.java
@@ -0,0 +1,49 @@
+package sehmus.school_management_system.model.concretes;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Meet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "HH:mm",timezone = "US")
+    private LocalTime startTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "HH:mm",timezone = "US")
+    private LocalTime stopTime;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User advisoryTeacher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "meet_student_table",
+            joinColumns = @JoinColumn(name = "meet_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<User> studentList;
+
+
+}
+
