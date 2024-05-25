6fd033b42b3d02262c4549139461694373947892
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:07:28 +0300

    Created LessonRequest Dto.

---
+++ src/main/java/sehmus/school_management_system/payload/requests/concretes/LessonRequest.java
@@ -0,0 +1,29 @@
+package sehmus.school_management_system.payload.requests.concretes;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LessonRequest {
+
+
+    @NotNull(message = "Please enter Lesson name")
+    @Size(min = 2, max = 16, message = "Lesson name should be at least 2 characters")
+    @Pattern(regexp = "\\A(?!\\s*\\Z).+" ,message="Lesson name must consist of the characters .")
+    private String lessonName;
+
+    @NotNull(message = "Please enter credit score")
+    private Integer creditScore;
+
+    @NotNull(message = "Please enter isCompulsory")
+    private Boolean isCompulsory;
+
+}
