6dce1b2ff1d13183203e6e4a890926e60bbfdc91
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 12:17:27 +0300

    StudentInfo class was created which contains general information about the student.

---
+++ src/main/java/sehmus/school_management_system/model/concretes/StudentInfo.java
@@ -0,0 +1,51 @@
+package sehmus.school_management_system.model.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sehmus.school_management_system.model.enums.Note;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer absentee;
+
+    private Double midtermExam;
+
+    private Double finalExam;
+
+    private Double examAverage;
+
+    private String infoNote;
+
+    @Enumerated(EnumType.STRING)
+    private Note letterGrade;
+
+    @JsonIgnore
+    @ManyToOne
+    private User teacher;
+
+    @JsonIgnore
+    @ManyToOne
+    private User student;
+
+    @ManyToOne
+    private Lesson lesson;
+
+    @OneToOne
+    private EducationTerm educationTerm;
+
+
+}
+
