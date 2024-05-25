ed03af07130c14106d8dfd31195c5c476f6bfde8
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:27:32 +0300

    Created TeacherResponse dto.

---
+++ src/main/java/sehmus/school_management_system/payload/responses/concretes/TeacherResponse.java
@@ -0,0 +1,24 @@
+package sehmus.school_management_system.payload.responses.concretes;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import sehmus.school_management_system.models.concretes.LessonProgram;
+import sehmus.school_management_system.payload.responses.abstracts.BaseUserResponse;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TeacherResponse extends BaseUserResponse {
+
+    private Set<LessonProgram> lessonProgramSet;
+
+    private Boolean isAdvisorTeacher;
+
+}
