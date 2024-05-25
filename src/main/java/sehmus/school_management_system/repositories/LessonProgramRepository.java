ccff4917f19b284b058d43789752e60ad29da854
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 13:08:22 +0300

    Created LessonProgramRepository interface for Database Connectivity.

---
+++ src/main/java/sehmus/school_management_system/repositories/LessonProgramRepository.java
@@ -0,0 +1,6 @@
+package sehmus.school_management_system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonProgramRepository  extends JpaRepository<LessonProgramRepository, Long> {
+}
