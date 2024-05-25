2d1546922e65ba39eb850bf6bd5b3667e23a11c3
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:08:29 +0300

    Created StudentRequest Dto.

---
+++ src/main/java/sehmus/school_management_system/payload/requests/concretes/StudentRequest.java
@@ -0,0 +1,30 @@
+package sehmus.school_management_system.payload.requests.concretes;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class StudentRequest extends BaseUserRequest {
+
+    @NotNull(message = "Please enter mother name")
+    @Size(min = 2, max = 16, message = "Your mother name should be at least 2 chars")
+    @Pattern(regexp="\\A(?!\\s*\\Z).+",message="Your mother name must consist of the characters a-z and 0-9.")
+    private String motherName;
+
+    @NotNull(message = "Please enter father name")
+    @Size(min = 2, max = 16, message = "Your father name should be at least 2 chars")
+    @Pattern(regexp="\\A(?!\\s*\\Z).+",message="Your father name must consist of the characters a-z and 0-9.")
+    private String fatherName;
+
+    @NotNull(message = "Please select advisor teacher")
+    private Long advisorTeacherId;
+
+}
