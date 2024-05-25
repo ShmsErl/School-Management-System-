8ce6a14c180418eb7f0f3c4dc9d947b45030ca02
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:26:33 +0300

    Created response class containing the message that will return to the requests sent  ResponseMessage dto

---
+++ src/main/java/sehmus/school_management_system/payload/responses/concretes/ResponseMessage.java
@@ -0,0 +1,21 @@
+package sehmus.school_management_system.payload.responses.concretes;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseMessage<T> {
+
+    private T returnBody;
+    private String message;
+    private HttpStatus httpStatus;
+
+}
