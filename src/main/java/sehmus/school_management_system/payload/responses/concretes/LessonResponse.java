ec3e5c4f0d1fbfe7cfac8ec0f71cdbb098ce5087
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:25:04 +0300

    Created LessonResponse dto.

---
+++ src/main/java/sehmus/school_management_system/payload/responses/concretes/LessonResponse.java
@@ -0,0 +1,19 @@
+package sehmus.school_management_system.payload.responses.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LessonResponse {
+
+    private Long lessonId;
+    private String lessonName;
+    private int creditScore;
+    private boolean isCompulsory;
+
+}
