f547b119a07f064777ee2bb33b97f301174c704c
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 11:26:45 +0300

    Created RoleType enum and User roll types have been defined for the user.

---
+++ src/main/java/sehmus/school_management_system/model/enums/RoleType.java
@@ -0,0 +1,19 @@
+package sehmus.school_management_system.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RoleType {
+
+    ADMIN("Admin"),
+    TEACHER("Teacher"),
+    STUDENT("Student"),
+    MANAGER("Dean"),
+    ASSISTANT_MANAGER("ViceDean");
+
+    public final String name;
+    RoleType(String name){
+        this.name = name;
+    }
+
+}
