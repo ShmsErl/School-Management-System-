399a30b669270bd6329138031495fc4193164929
ShmsErl <erol.sehmus1@gmail.com>
2024-05-25 14:53:32 +0300

    Created Derived Querry , that name getByLessonNameEqualsIgnoreCase.

--- src/main/java/sehmus/school_management_system/repositories/LessonRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/LessonRepository.java
@@ -4,5 +4,8 @@ package sehmus.school_management_system.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sehmus.school_management_system.models.concretes.Lesson;
 
+import java.util.Optional;
+
 public interface  LessonRepository extends JpaRepository<Lesson,Long> {
+    Optional<Lesson> getByLessonNameEqualsIgnoreCase(String lessonName);
 }
