04850104019dc87f6492d7c92edf2214faae10c1
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:35:00 +0300

    Your StudentInfoRepository interface defines several custom queries: giveMeDuplications: Checks for duplications in student info based on student ID and lesson name. findByStudentId: Retrieves student info entries based on student ID. findByTeacherUsername: Retrieves student info entries based on teacher's username, with pagination. findByStudentUsername: Retrieves student info entries based on student's username, with pagination.

--- src/main/java/sehmus/school_management_system/repositories/StudentInfoRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/StudentInfoRepository.java
@@ -1,7 +1,24 @@
 package sehmus.school_management_system.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sehmus.school_management_system.models.concretes.StudentInfo;
 
+import java.util.List;
+
 public interface StudentInfoRepository extends JpaRepository<StudentInfo,Long> {
+
+    @Query("SELECT (count (s)>0) FROM StudentInfo s WHERE s.student.id = ?1 AND s.lesson.lessonName = ?2")
+    boolean giveMeDuplications(Long studentId, String lessonName);
+
+    @Query("SELECT s FROM StudentInfo s WHERE s.student.id = ?1")
+    List<StudentInfo> findByStudentId(Long studentId);
+
+    @Query("SELECT s FROM StudentInfo s WHERE s.teacher.username=?1")
+    Page<StudentInfo> findByTeacherUsername(String username, Pageable pageable);
+
+    @Query("SELECT s FROM StudentInfo s WHERE s.student.username=?1")
+    Page<StudentInfo> findByStudentUsername(String username, Pageable pageable);
 }
