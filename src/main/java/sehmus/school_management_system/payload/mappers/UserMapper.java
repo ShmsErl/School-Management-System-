0635bc0f860c5b2f3ccb31ff3dc01162ef1163e5
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:13:01 +0300

    Add method to map user to user response. Implemented mapUserToUserResponse to map user entity to user response, including user ID, username, name, surname, phone number, gender, birthdate, birthplace, SSN, email, and user role. This enables conversion of user data for response generation.

--- src/main/java/sehmus/school_management_system/payload/mappers/UserMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/UserMapper.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import sehmus.school_management_system.models.concretes.User;
 import sehmus.school_management_system.payload.requests.abstracts.BaseUserRequest;
+import sehmus.school_management_system.payload.responses.concretes.UserResponse;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +28,24 @@ public class UserMapper {
                 .email(userRequest.getEmail())
                 .builtIn(userRequest.getBuiltIn())
                 .isAdvisor(false)
+                .build();
+
+    }
+
+    public UserResponse mapUserToUserResponse(User user){
+
+        return UserResponse.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .phoneNumber(user.getPhoneNumber())
+                .gender(user.getGender())
+                .birthDay(user.getBirthDay())
+                .birthPlace(user.getBirthPlace())
+                .ssn(user.getSsn())
+                .email(user.getEmail())
+                .userRole(user.getUserRole().getRoleType().name)
                 .build();
 
     }
