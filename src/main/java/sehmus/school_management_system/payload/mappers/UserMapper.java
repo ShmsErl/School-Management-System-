bf051b4fe0251d247dc875c637d6bc05454d0dfc
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:12:34 +0300

    Implement method to map user request to user entity. Implemented mapUserRequestToUser to map user request data to user entity, including username, name, surname, encoded password, SSN, birthdate, birthplace, phone number, gender, email, and other properties. This facilitates user creation and registration.

--- src/main/java/sehmus/school_management_system/payload/mappers/UserMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/UserMapper.java
@@ -1,4 +1,33 @@
 package sehmus.school_management_system.payload.mappers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import sehmus.school_management_system.models.concretes.User;
+import sehmus.school_management_system.payload.requests.abstracts.BaseUserRequest;
+
+@Component
+@RequiredArgsConstructor
 public class UserMapper {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    public User mapUserRequestToUser(BaseUserRequest userRequest){
+        return User.builder()
+                .username(userRequest.getUsername())
+                .name(userRequest.getName())
+                .surname(userRequest.getSurname())
+                .password(passwordEncoder.encode(userRequest.getPassword()))
+                .ssn(userRequest.getSsn())
+                .birthDay(userRequest.getBirthDay())
+                .birthPlace(userRequest.getBirthPlace())
+                .phoneNumber(userRequest.getPhoneNumber())
+                .gender(userRequest.getGender())
+                .email(userRequest.getEmail())
+                .builtIn(userRequest.getBuiltIn())
+                .isAdvisor(false)
+                .build();
+
+    }
 }
