9a00edc3e598a9e95944f9f0e3fa78e32c35afeb
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 10:45:10 +0300

    Jpa query queries were created to check year and period information.

--- src/main/java/sehmus/school_management_system/repositories/EducationTermRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/EducationTermRepository.java
@@ -1,7 +1,17 @@
 package sehmus.school_management_system.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sehmus.school_management_system.models.concretes.EducationTerm;
+import sehmus.school_management_system.models.enums.Term;
+
+import java.util.List;
 
 public interface EducationTermRepository extends JpaRepository<EducationTerm, Long> {
+
+    @Query("SELECT (count (e) > 0) FROM EducationTerm e WHERE e.term =?1 AND extract(YEAR FROM e.startDate) =?2")
+    boolean existsByTermAndYear(Term term, int year);
+
+    @Query("SELECT e FROM EducationTerm e WHERE extract(YEAR FROM e.startDate) =?1")
+    List<EducationTerm> findByYear(int year);
 }
