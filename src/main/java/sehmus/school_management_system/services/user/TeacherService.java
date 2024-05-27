c8051518bc21ce7fc9572fe3d3372de54c3037cb
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 15:55:39 +0300

    The saveTeacher method saves a new teacher's information, ensuring that properties like username, SSN, phone number, and email are unique, and assigns the appropriate role and lesson programs before saving the teacher to the repository. It returns a response message with the saved teacher's details and a success message.

--- src/main/java/sehmus/school_management_system/services/user/TeacherService.java
+++ src/main/java/sehmus/school_management_system/services/user/TeacherService.java
@@ -2,14 +2,25 @@ package sehmus.school_management_system.services.user;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sehmus.school_management_system.models.concretes.LessonProgram;
+import sehmus.school_management_system.models.concretes.User;
+import sehmus.school_management_system.models.enums.RoleType;
 import sehmus.school_management_system.payload.mappers.UserMapper;
+import sehmus.school_management_system.payload.messages.SuccessMessages;
+import sehmus.school_management_system.payload.requests.concretes.TeacherRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.UserResponse;
 import sehmus.school_management_system.repositories.UserRepository;
 import sehmus.school_management_system.services.business.LessonProgramService;
 import sehmus.school_management_system.services.helper.MethodHelper;
 import sehmus.school_management_system.services.validator.DateTimeValidator;
 import sehmus.school_management_system.services.validator.UniquePropertyValidator;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +41,39 @@ public class TeacherService {
     private final MethodHelper methodHelper;
 
     private final DateTimeValidator dateTimeValidator;
+
+
+
+
+
+
+    @Transactional
+    public ResponseMessage<UserResponse> saveTeacher(TeacherRequest teacherRequest) {
+
+        Set<LessonProgram> lessonProgramSet =
+                lessonProgramService.getLessonProgramById(teacherRequest.getLessonProgramIdList());
+
+        uniquePropertyValidator.checkDuplicate(
+                teacherRequest.getUsername(),
+                teacherRequest.getSsn(),
+                teacherRequest.getPhoneNumber(),
+                teacherRequest.getEmail()
+        );
+
+        User teacher = userMapper.mapUserRequestToUser(teacherRequest);
+
+        teacher.setIsAdvisor(teacherRequest.getIsAdvisorTeacher());
+        teacher.setLessonProgramList(lessonProgramSet);
+        teacher.setUserRole(userRoleService.getUserRole(RoleType.TEACHER));
+
+        User savedTeacher = userRepository.save(teacher);
+
+        return ResponseMessage.<UserResponse>builder()
+                .message(SuccessMessages.TEACHER_SAVE)
+                .httpStatus(HttpStatus.CREATED)
+                .returnBody(userMapper.mapUserToUserResponse(savedTeacher))
+                .build();
+    }
 
 
 }
