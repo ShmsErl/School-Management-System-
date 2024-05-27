facdc51760754d87b9e76946056db13dcad3bb22
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:09:53 +0300

    Add method to map student info to student info response. Implemented mapStudentInfoToStudentInfoResponse to map student info entity to student info response, including lesson details, term, absenteeism, exam scores, notes, average, and student details. This enables conversion of student info data for response generation.

--- src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import sehmus.school_management_system.models.concretes.StudentInfo;
 import sehmus.school_management_system.models.enums.Note;
 import sehmus.school_management_system.payload.requests.concretes.StudentInfoRequest;
+import sehmus.school_management_system.payload.responses.concretes.StudentInfoResponse;
 
 @Component
 @Data
@@ -14,7 +15,7 @@ import sehmus.school_management_system.payload.requests.concretes.StudentInfoReq
 public class StudentInfoMapper {
 
 
-    //private final UserMapper userMapper;
+    private final UserMapper userMapper;
 
     public StudentInfo mapStudentInfoRequestToStudentInfo(StudentInfoRequest studentInfoRequest,
                                                           Note note, Double average){
@@ -28,6 +29,24 @@ public class StudentInfoMapper {
                 .letterGrade(note)
                 .build();
 
+    }
+
+    public StudentInfoResponse mapStudentInfoToStudentInfoResponse (StudentInfo studentInfo){
+
+        return StudentInfoResponse.builder()
+                .lessonName(studentInfo.getLesson().getLessonName())
+                .creditScore(studentInfo.getLesson().getCreditScore())
+                .isCompulsory(studentInfo.getLesson().getIsCompulsory())
+                .educationTerm(studentInfo.getEducationTerm().getTerm())
+                .id(studentInfo.getId())
+                .absentee(studentInfo.getAbsentee())
+                .midtermExam(studentInfo.getMidtermExam())
+                .finalExam(studentInfo.getFinalExam())
+                .infoNote(studentInfo.getInfoNote())
+                .note(studentInfo.getLetterGrade())
+                .average(studentInfo.getExamAverage())
+                .studentResponse(userMapper.mapUserToStudentResponse(studentInfo.getStudent()))
+                .build();
     }
 
 }
