7b2ff7c738cb06682ac803d1fb508ff810555364
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 11:11:23 +0300

    Created StudentInfoService class.

---
+++ src/main/java/sehmus/school_management_system/services/business/StudentInfoService.java
@@ -0,0 +1,10 @@
+package sehmus.school_management_system.services.business;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentInfoService {
+
+}
