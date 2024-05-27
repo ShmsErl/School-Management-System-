8b20d5cf77209413ad508633abd7478aa4dbfe4c
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 10:58:58 +0300

    The mapMeetToMeetingResponse function was defined for the map operation.

--- src/main/java/sehmus/school_management_system/payload/mappers/MeetingMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/MeetingMapper.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 import sehmus.school_management_system.models.concretes.Meet;
 import sehmus.school_management_system.payload.requests.concretes.MeetingRequest;
+import sehmus.school_management_system.payload.responses.concretes.MeetingResponse;
 
 @Component
 @Data
@@ -17,6 +18,22 @@ public class MeetingMapper {
                 .startTime(meetingRequest.getStartTime())
                 .stopTime(meetingRequest.getStopTime())
                 .description(meetingRequest.getDescription())
+                .build();
+
+    }
+
+    public MeetingResponse mapMeetToMeetingResponse(Meet meet){
+
+        return MeetingResponse.builder()
+                .id(meet.getId())
+                .date(meet.getDate())
+                .startTime(meet.getStartTime())
+                .stopTime(meet.getStopTime())
+                .description(meet.getDescription())
+                .advisorTeacherId(meet.getAdvisoryTeacher().getId())
+                .teacherSsn(meet.getAdvisoryTeacher().getSsn())
+                .teacherName(meet.getAdvisoryTeacher().getName())
+                .students(meet.getStudentList())
                 .build();
 
     }
