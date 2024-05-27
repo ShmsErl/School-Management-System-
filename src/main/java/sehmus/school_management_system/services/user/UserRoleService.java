b7e0285ec877f2d52695ade779703fb3fbbe7a79
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:41:37 +0300

    Created UserRoleService class.

---
+++ src/main/java/sehmus/school_management_system/services/user/UserRoleService.java
@@ -0,0 +1,9 @@
+package sehmus.school_management_system.services.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRoleService {
+}
