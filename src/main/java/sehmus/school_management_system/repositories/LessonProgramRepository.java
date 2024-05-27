1c75f5a3b0c8a3076dc43bebf4dcda76a25ad9f7
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 10:46:09 +0300

    Database queries were created for use in the business layer.

--- src/main/java/sehmus/school_management_system/repositories/LessonProgramRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/LessonProgramRepository.java
@@ -1,6 +1,24 @@
 package sehmus.school_management_system.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sehmus.school_management_system.models.concretes.LessonProgram;
 
-public interface LessonProgramRepository  extends JpaRepository<LessonProgramRepository, Long> {
+import java.util.List;
+import java.util.Set;
+
+public interface LessonProgramRepository  extends JpaRepository<LessonProgram, Long> {
+
+    List<LessonProgram> findByUsers_IdNull();
+
+    List<LessonProgram> findByUsers_IdNotNull();
+
+    @Query("SELECT l FROM LessonProgram l WHERE l.id IN :idSet")
+    Set<LessonProgram> getLessonProgramByIdList(@Param("idSet") Set<Long> idSet);
+
+    @Query("SELECT l FROM LessonProgram l INNER JOIN l.users users WHERE users.username =?1")
+    Set<LessonProgram> getLessonProgramByUsername(String username);
+
+    Set<LessonProgram>findByUsers_IdEquals(Long userId);
 }
