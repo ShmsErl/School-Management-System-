693b3f5350e128f42de104bafe6a321e58d037b9
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 10:55:34 +0300

    Created MeetingService class.

---
+++ src/main/java/sehmus/school_management_system/services/business/MeetingService.java
@@ -0,0 +1,10 @@
+package sehmus.school_management_system.services.business;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MeetingService {
+
+}
