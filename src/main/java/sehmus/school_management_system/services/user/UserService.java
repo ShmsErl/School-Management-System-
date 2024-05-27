5d8eb9807e4d690ec4d3deec9669b7f1d36a38bd
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 15:15:25 +0300

    Created UserService class.

---
+++ src/main/java/sehmus/school_management_system/services/user/UserService.java
@@ -0,0 +1,11 @@
+package sehmus.school_management_system.services.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
+}
