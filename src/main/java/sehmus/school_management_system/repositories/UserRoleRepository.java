4bd8e7097f942cd125f4b964542c16de7164484d
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 15:04:33 +0300

    Created Querry findByEnumRoleEquals in the UserRoleRepository.

--- src/main/java/sehmus/school_management_system/repositories/UserRoleRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/UserRoleRepository.java
@@ -1,8 +1,14 @@
 package sehmus.school_management_system.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sehmus.school_management_system.models.concretes.UserRole;
+import sehmus.school_management_system.models.enums.RoleType;
+
+import java.util.Optional;
 
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
 
+    @Query("SELECT r FROM UserRole r WHERE r.roleType = ?1")
+    Optional<UserRole> findByEnumRoleEquals(RoleType roleType);
 }
