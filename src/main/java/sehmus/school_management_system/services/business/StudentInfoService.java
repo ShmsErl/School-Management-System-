738694f4abcbb759bb0b16288b3553778a28d72c
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:28:52 +0300

    Created SaveStudentInfo method and Implement method to save student info. Implemented saveStudentInfo to handle saving student information, including validating roles and existence of student, teacher, lesson, and education term. Calculates exam average and letter grade, maps request data to student info entity, and saves it to the repository. Returns a response message with the saved student info details.

--- src/main/java/sehmus/school_management_system/services/business/StudentInfoService.java
+++ src/main/java/sehmus/school_management_system/services/business/StudentInfoService.java
@@ -1,10 +1,80 @@
 package sehmus.school_management_system.services.business;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import sehmus.school_management_system.models.concretes.EducationTerm;
+import sehmus.school_management_system.models.concretes.Lesson;
+import sehmus.school_management_system.models.concretes.StudentInfo;
+import sehmus.school_management_system.models.concretes.User;
+import sehmus.school_management_system.models.enums.Note;
+import sehmus.school_management_system.models.enums.RoleType;
+import sehmus.school_management_system.payload.mappers.StudentInfoMapper;
+import sehmus.school_management_system.payload.messages.SuccessMessages;
+import sehmus.school_management_system.payload.requests.concretes.StudentInfoRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.StudentInfoResponse;
+import sehmus.school_management_system.repositories.StudentInfoRepository;
+import sehmus.school_management_system.services.helper.MethodHelper;
+import sehmus.school_management_system.services.helper.PageableHelper;
 
 @Service
 @RequiredArgsConstructor
 public class StudentInfoService {
+
+    private final StudentInfoRepository studentInfoRepository;
+
+    private final MethodHelper methodHelper;
+
+    private final LessonService lessonService;
+
+    private final EducationTermService educationTermService;
+
+    private final StudentInfoMapper studentInfoMapper;
+
+    private final PageableHelper pageableHelper;
+
+    @Value("${midterm.exam.impact.percentage}")
+    private Double midTermExamPercentage;
+    @Value("${final.exam.impact.percentage}")
+    private Double finalExamPercentage;
+
+
+    public ResponseMessage<StudentInfoResponse> saveStudentInfo(HttpServletRequest httpServletRequest,
+                                                                StudentInfoRequest studentInfoRequest) {
+
+        String teacherUsername = (String) httpServletRequest.getAttribute("username");
+
+        User student = methodHelper.isUserExist(studentInfoRequest.getStudentId());
+        methodHelper.checkRole(student, RoleType.STUDENT);
+        User teacher = methodHelper.loadUserByName(teacherUsername);
+        Lesson lesson = lessonService.isLessonExistById(studentInfoRequest.getLessonId());
+        EducationTerm educationTerm = educationTermService.isEducationTermExists(
+                studentInfoRequest.getEducationTermId());
+        validateLessonDuplication(studentInfoRequest.getStudentId(), lesson.getLessonName());
+        Note note = checkLetterGrade(calculateExamAverage(studentInfoRequest.getMidtermExam(),
+                studentInfoRequest.getFinalExam()));
+
+        StudentInfo studentInfo = studentInfoMapper.mapStudentInfoRequestToStudentInfo(
+                studentInfoRequest,
+                note,
+                calculateExamAverage(studentInfoRequest.getMidtermExam(),
+                        studentInfoRequest.getFinalExam())
+        );
+        studentInfo.setStudent(student);
+        studentInfo.setEducationTerm(educationTerm);
+        studentInfo.setTeacher(teacher);
+        studentInfo.setLesson(lesson);
+        StudentInfo savedStudentInfo = studentInfoRepository.save(studentInfo);
+
+        return ResponseMessage.<StudentInfoResponse>builder()
+                .message(SuccessMessages.STUDENT_INFO_SAVE)
+                .returnBody(studentInfoMapper.mapStudentInfoToStudentInfoResponse(savedStudentInfo))
+                .httpStatus(HttpStatus.OK)
+                .build();
+
+    }
 
 }
