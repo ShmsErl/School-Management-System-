18312d2dc555e226ec166cdd74ff187cd14fe7de
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 11:01:47 +0300

    Add method to validate if meeting matches the teacher. Implemented isMeetingMatchedWithTeacher to verify the advisory teacher's identity based on the logged-in user's username, ensuring the teacher is an advisor and throwing a BadRequestException if the meeting does not match the teacher. This enhances security and enforces proper permissions for meeting management.

--- src/main/java/sehmus/school_management_system/services/helper/MeetingHelper.java
+++ src/main/java/sehmus/school_management_system/services/helper/MeetingHelper.java
@@ -1,7 +1,12 @@
 package sehmus.school_management_system.services.helper;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import sehmus.school_management_system.exception.BadRequestException;
+import sehmus.school_management_system.models.concretes.Meet;
+import sehmus.school_management_system.models.concretes.User;
+import sehmus.school_management_system.payload.messages.ErrorMessages;
 import sehmus.school_management_system.repositories.MeetRepository;
 
 @RequiredArgsConstructor
@@ -10,4 +15,20 @@ public class MeetingHelper {
     private final MethodHelper methodHelper;
 
     private final MeetRepository meetingRepository;
+
+
+
+    public void isMeetingMatchedWithTeacher(Meet meet, HttpServletRequest httpServletRequest){
+
+        String username = (String) httpServletRequest.getAttribute("username");
+        User teacher = methodHelper.loadUserByName(username);
+        methodHelper.checkIsAdvisor(teacher);
+
+        if (meet.getAdvisoryTeacher().getId().equals(teacher.getId())){
+
+            throw new BadRequestException(ErrorMessages.NOT_PERMITTED_METHOD_MESSAGE);
+
+        }
+
+    }
 }
