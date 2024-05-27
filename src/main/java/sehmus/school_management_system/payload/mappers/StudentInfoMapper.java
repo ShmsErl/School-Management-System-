5a772b6e0d10a18b1cbd8db89cec352286dfa7aa
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:09:02 +0300

    Implement method to map student info request to student info. Implemented mapStudentInfoRequestToStudentInfo to map student info request data to student info entity, including notes, absenteeism, exam scores, average, and letter grade. This facilitates data transformation for student information management.

--- src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
@@ -4,6 +4,9 @@ package sehmus.school_management_system.payload.mappers;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import sehmus.school_management_system.models.concretes.StudentInfo;
+import sehmus.school_management_system.models.enums.Note;
+import sehmus.school_management_system.payload.requests.concretes.StudentInfoRequest;
 
 @Component
 @Data
@@ -11,5 +14,20 @@ import org.springframework.stereotype.Component;
 public class StudentInfoMapper {
 
 
+    //private final UserMapper userMapper;
+
+    public StudentInfo mapStudentInfoRequestToStudentInfo(StudentInfoRequest studentInfoRequest,
+                                                          Note note, Double average){
+
+        return StudentInfo.builder()
+                .infoNote(studentInfoRequest.getInfoNote())
+                .absentee(studentInfoRequest.getAbsentee())
+                .midtermExam(studentInfoRequest.getMidtermExam())
+                .finalExam(studentInfoRequest.getFinalExam())
+                .examAverage(average)
+                .letterGrade(note)
+                .build();
+
+    }
 
 }
