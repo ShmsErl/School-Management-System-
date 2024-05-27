3261bb4b76083444031ae74d776c18d2d412f269
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 14:08:06 +0300

    Created StudentInfoMapper class.

--- src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/StudentInfoMapper.java
@@ -1,4 +1,15 @@
 package sehmus.school_management_system.payload.mappers;
 
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@RequiredArgsConstructor
 public class StudentInfoMapper {
+
+
+
 }
