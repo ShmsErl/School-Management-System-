ae2a3e860d189596c180ca5635676217d041a294
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 16:08:24 +0300

    Add method to save a new student with validation and advisor teacher assignment

--- src/main/java/sehmus/school_management_system/services/user/StudentService.java
+++ src/main/java/sehmus/school_management_system/services/user/StudentService.java
@@ -2,9 +2,16 @@ package sehmus.school_management_system.services.user;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import sehmus.school_management_system.models.concretes.User;
+import sehmus.school_management_system.models.enums.RoleType;
 import sehmus.school_management_system.payload.mappers.UserMapper;
+import sehmus.school_management_system.payload.messages.SuccessMessages;
+import sehmus.school_management_system.payload.requests.concretes.StudentRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.StudentResponse;
 import sehmus.school_management_system.repositories.UserRepository;
 import sehmus.school_management_system.services.business.LessonProgramService;
 import sehmus.school_management_system.services.helper.MethodHelper;
@@ -31,4 +38,38 @@ public class StudentService {
     private final MethodHelper methodHelper;
 
     private final UserMapper userMapper;
+
+
+
+    public ResponseMessage<StudentResponse> saveStudent(StudentRequest studentRequest) {
+
+        User advisorTeacher = methodHelper.isUserExist(studentRequest.getAdvisorTeacherId());
+
+        methodHelper.checkIsAdvisor(advisorTeacher);
+
+        uniquePropertyValidator.checkDuplicate(
+                studentRequest.getUsername(),
+                studentRequest.getSsn(),
+                studentRequest.getPhoneNumber(),
+                studentRequest.getEmail()
+
+        );
+
+        User student = userMapper.mapUserRequestToUser(studentRequest);
+
+        student.setAdvisorTeacherId(advisorTeacher.getId());
+        student.setUserRole(userRoleService.getUserRole(RoleType.STUDENT));
+        student.setActive(true);
+        student.setIsAdvisor(false);
+        student.setStudentNumber(getLastNumber());
+
+
+        return ResponseMessage.<StudentResponse>builder()
+                .returnBody(userMapper.mapUserToStudentResponse(userRepository.save(student)))
+                .message(SuccessMessages.STUDENT_SAVE)
+                .httpStatus(HttpStatus.OK)
+                .build();
+
+    }
+
 }
