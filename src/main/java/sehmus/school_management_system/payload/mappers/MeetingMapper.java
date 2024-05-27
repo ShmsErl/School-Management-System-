9a30bb7337e60beb6f216dc607edd8b3b39f0279
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 10:58:27 +0300

    The mapMeetingRequestToMeet function was defined for the map operation.

--- src/main/java/sehmus/school_management_system/payload/mappers/MeetingMapper.java
+++ src/main/java/sehmus/school_management_system/payload/mappers/MeetingMapper.java
@@ -1,4 +1,23 @@
 package sehmus.school_management_system.payload.mappers;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import sehmus.school_management_system.models.concretes.Meet;
+import sehmus.school_management_system.payload.requests.concretes.MeetingRequest;
+
+@Component
+@Data
 public class MeetingMapper {
+
+
+    public Meet mapMeetingRequestToMeet(MeetingRequest meetingRequest){
+
+        return Meet.builder()
+                .date(meetingRequest.getDate())
+                .startTime(meetingRequest.getStartTime())
+                .stopTime(meetingRequest.getStopTime())
+                .description(meetingRequest.getDescription())
+                .build();
+
+    }
 }
