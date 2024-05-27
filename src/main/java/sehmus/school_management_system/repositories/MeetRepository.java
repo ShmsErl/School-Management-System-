0dd1ede95faa5d205ec83edd4192f6fe0ec7b210
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 11:03:48 +0300

    Add repository methods for retrieving meetings. Implemented methods to find meetings by student ID (findByStudentList_IdEquals), advisory teacher ID (getByAdvisoryTeacher_IdEquals), and paginated meetings by advisory teacher ID (findByAdvisoryTeacher_IdEquals with Pageable). These methods facilitate efficient querying of meetings based on participants and advisors.

--- src/main/java/sehmus/school_management_system/repositories/MeetRepository.java
+++ src/main/java/sehmus/school_management_system/repositories/MeetRepository.java
@@ -1,7 +1,17 @@
 package sehmus.school_management_system.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sehmus.school_management_system.models.concretes.Meet;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface MeetRepository extends JpaRepository<Meet, Long> {
+    List<Meet> findByStudentList_IdEquals(Long studentId);
+
+    List<Meet> getByAdvisoryTeacher_IdEquals(Long advisoryTeacherId);
+
+    Page<Meet> findByAdvisoryTeacher_IdEquals(Long advisoryTeacherId, Pageable pageable);;
 }
