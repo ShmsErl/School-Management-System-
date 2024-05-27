8223748938d24488411d592946666a6c87dd7192
ShmsErl <erol.sehmus1@gmail.com>
2024-05-27 15:05:20 +0300

    Your service contains methods related to user roles: getUserRole: Retrieves a user role by its role type. It queries the UserRoleRepository to find a user role corresponding to the provided RoleType. If the role is found, it returns it; otherwise, it throws a ResourceNotFoundException. getAllUserRole: Retrieves all user roles. It simply queries the UserRoleRepository to retrieve all user roles present in the database and returns them as a list.

--- src/main/java/sehmus/school_management_system/services/user/UserRoleService.java
+++ src/main/java/sehmus/school_management_system/services/user/UserRoleService.java
@@ -2,8 +2,28 @@ package sehmus.school_management_system.services.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sehmus.school_management_system.exception.ResourceNotFoundException;
+import sehmus.school_management_system.models.concretes.UserRole;
+import sehmus.school_management_system.models.enums.RoleType;
+import sehmus.school_management_system.payload.messages.ErrorMessages;
+import sehmus.school_management_system.repositories.UserRoleRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserRoleService {
+
+    private final UserRoleRepository userRoleRepository;
+
+    public UserRole getUserRole(RoleType roleType){
+        return userRoleRepository.findByEnumRoleEquals(roleType)
+                .orElseThrow(()-> new ResourceNotFoundException(ErrorMessages.ROLE_NOT_FOUND));
+
+    }
+
+    public List<UserRole> getAllUserRole(){
+        return userRoleRepository.findAll();
+
+    }
 }
