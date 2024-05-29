5a21cf6ac1ed8c7ce7ce5c6a476f69a2612b3a6e
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 00:14:46 +0300

    Added AuthenticationManager bean for JWT authentication.

--- src/main/java/sehmus/school_management_system/security/config/WebSecurityConfig.java
+++ src/main/java/sehmus/school_management_system/security/config/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package sehmus.school_management_system.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import sehmus.school_management_system.security.jwt.AuthTokenFilter;
@@ -17,7 +19,14 @@ public class WebSecurityConfig {
 
 
     @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter(){
+    public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+
+    }
 }
