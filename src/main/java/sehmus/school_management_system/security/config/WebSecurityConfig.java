402e4ac829eb13b538aad32552cab1090ae8640f
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 00:13:57 +0300

    Added AuthTokenFilter bean for JWT authentication.

--- src/main/java/sehmus/school_management_system/security/config/WebSecurityConfig.java
+++ src/main/java/sehmus/school_management_system/security/config/WebSecurityConfig.java
@@ -1,14 +1,23 @@
 package sehmus.school_management_system.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import sehmus.school_management_system.security.jwt.AuthTokenFilter;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+
+
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
 
 }
