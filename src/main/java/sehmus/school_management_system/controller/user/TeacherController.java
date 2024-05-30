3cef2c7d7be5faf290fc298c113346b459ca1d8e
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 20:50:35 +0300

    Added /saveTeacher endpoint .

--- src/main/java/sehmus/school_management_system/controller/user/TeacherController.java
+++ src/main/java/sehmus/school_management_system/controller/user/TeacherController.java
@@ -1,8 +1,16 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.TeacherRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.UserResponse;
 import sehmus.school_management_system.services.user.TeacherService;
 
 @RestController
@@ -11,4 +19,11 @@ import sehmus.school_management_system.services.user.TeacherService;
 public class TeacherController {
 
     private final TeacherService teacherService;
+
+    @PreAuthorize("hasAnyAuthority('Admin')")
+    @PostMapping("/save")
+    public ResponseEntity<ResponseMessage<UserResponse>> saveTeacher(@Valid @RequestBody TeacherRequest teacherRequest){
+
+        return ResponseEntity.ok(teacherService.saveTeacher(teacherRequest));
+    }
 }
