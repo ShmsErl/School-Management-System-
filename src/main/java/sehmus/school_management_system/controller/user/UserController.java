ac84652da92b1fc529d29449c1b0abd8ec27bfb3
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 19:52:03 +0300

    Added /getUserByPage enpoint in the UserController class.

--- src/main/java/sehmus/school_management_system/controller/user/UserController.java
+++ src/main/java/sehmus/school_management_system/controller/user/UserController.java
@@ -2,6 +2,8 @@ package sehmus.school_management_system.controller.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +28,22 @@ public class UserController {
         return ResponseEntity.ok(userService.saveUser(userRequest, userRole));
 
     }
+
+    @PreAuthorize(("hasAnyAuthority('Admin')"))
+    @GetMapping("/getAllUsersByPage/{userRole}")
+    public ResponseEntity<Page<UserResponse>> getUserByPage(
+            @PathVariable String userRole,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "name") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type
+    ){
+
+        Page<UserResponse> userResponse = userService.getUsersByPage(page,size,sort,type,userRole);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+
+    }
+
 
 
 }
