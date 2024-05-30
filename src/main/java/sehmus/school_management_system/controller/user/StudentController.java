9784a3884d7ff8c1c04cb8f9df719fed1d10f07e
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 20:45:44 +0300

    Added /saveStudent endpoint.

--- src/main/java/sehmus/school_management_system/controller/user/StudentController.java
+++ src/main/java/sehmus/school_management_system/controller/user/StudentController.java
@@ -1,8 +1,16 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.StudentRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.StudentResponse;
 import sehmus.school_management_system.services.user.StudentService;
 
 @RestController
@@ -11,5 +19,13 @@ import sehmus.school_management_system.services.user.StudentService;
 public class StudentController {
     private final StudentService studentService;
 
+
+    @PreAuthorize("hasAnyAuthority('Admin')")
+    @PostMapping("/save")
+    public ResponseEntity<ResponseMessage<StudentResponse>> saveStudent(@Valid @RequestBody StudentRequest studentRequest){
+
+        return ResponseEntity.ok(studentService.saveStudent(studentRequest));
+
+    }
 
 }
