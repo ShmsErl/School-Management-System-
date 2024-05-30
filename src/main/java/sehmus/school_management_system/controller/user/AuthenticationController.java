f4710d9649c3da9cf78244063eb842366c981c9b
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 19:47:14 +0300

    Added /login endpoint to Authentication Controller.

--- src/main/java/sehmus/school_management_system/controller/user/AuthenticationController.java
+++ src/main/java/sehmus/school_management_system/controller/user/AuthenticationController.java
@@ -1,8 +1,14 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.authentication.LoginRequest;
+import sehmus.school_management_system.payload.responses.authentication.AuthResponse;
 import sehmus.school_management_system.services.user.AuthenticationService;
 
 @RestController
@@ -11,4 +17,11 @@ import sehmus.school_management_system.services.user.AuthenticationService;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticateUser (@RequestBody @Valid LoginRequest loginRequest){
+
+        return ResponseEntity.ok(authenticationService.authenticateUser(loginRequest));
+
+    }
 }
