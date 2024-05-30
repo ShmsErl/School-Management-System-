0c2518ea2fa0ea73d4dfe272df5dbd8a416978c4
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 19:48:04 +0300

    Added /updatePassword endpoint to Authentication Controller.

--- src/main/java/sehmus/school_management_system/controller/user/AuthenticationController.java
+++ src/main/java/sehmus/school_management_system/controller/user/AuthenticationController.java
@@ -1,13 +1,14 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import sehmus.school_management_system.payload.messages.SuccessMessages;
 import sehmus.school_management_system.payload.requests.authentication.LoginRequest;
+import sehmus.school_management_system.payload.requests.authentication.UpdatePasswordRequest;
 import sehmus.school_management_system.payload.responses.authentication.AuthResponse;
 import sehmus.school_management_system.services.user.AuthenticationService;
 
@@ -22,6 +23,17 @@ public class AuthenticationController {
     public ResponseEntity<AuthResponse> authenticateUser (@RequestBody @Valid LoginRequest loginRequest){
 
         return ResponseEntity.ok(authenticationService.authenticateUser(loginRequest));
+
+    }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher','Student')")
+    @PatchMapping("/updatePassword")
+    public ResponseEntity<String> updatePassword(@Valid @RequestBody UpdatePasswordRequest updatePasswordRequest,
+                                                 HttpServletRequest httpServletRequest){
+
+        authenticationService.updatePassword(updatePasswordRequest, httpServletRequest);
+
+        return ResponseEntity.ok(SuccessMessages.PASSWORD_CHANGED_RESPONSE_MESSAGE);
 
     }
 }
