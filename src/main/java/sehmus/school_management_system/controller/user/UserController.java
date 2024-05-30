4a5f1c9b279a4493ab68b177b0f1b85ba8ad8026
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 19:51:27 +0300

    Added /saveUser enpoint in the UserController class.

--- src/main/java/sehmus/school_management_system/controller/user/UserController.java
+++ src/main/java/sehmus/school_management_system/controller/user/UserController.java
@@ -1,8 +1,13 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import sehmus.school_management_system.payload.requests.concretes.UserRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.UserResponse;
 import sehmus.school_management_system.services.user.UserService;
 
 @RestController
@@ -10,4 +15,17 @@ import sehmus.school_management_system.services.user.UserService;
 @RequiredArgsConstructor
 public class UserController {
     private  final UserService userService;
+
+    @PreAuthorize(("hasAnyAuthority('Admin')"))
+    @PostMapping("/save/{userRole}")
+    public ResponseEntity<ResponseMessage<UserResponse>> saveUser(
+            @RequestBody @Valid UserRequest userRequest,
+            @PathVariable String userRole
+    ){
+
+        return ResponseEntity.ok(userService.saveUser(userRequest, userRole));
+
+    }
+
+
 }
