d259fe6b3e23af98b941f571a4baffb9a5d8593a
ShmsErl <erol.sehmus1@gmail.com>
2024-05-30 20:46:30 +0300

    Added /updateStudent endpoint.

--- src/main/java/sehmus/school_management_system/controller/user/StudentController.java
+++ src/main/java/sehmus/school_management_system/controller/user/StudentController.java
@@ -1,14 +1,13 @@
 package sehmus.school_management_system.controller.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sehmus.school_management_system.payload.requests.concretes.StudentRequest;
+import sehmus.school_management_system.payload.requests.concretes.StudentUpdateRequestWithoutPassword;
 import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
 import sehmus.school_management_system.payload.responses.concretes.StudentResponse;
 import sehmus.school_management_system.services.user.StudentService;
@@ -25,6 +24,16 @@ public class StudentController {
     public ResponseEntity<ResponseMessage<StudentResponse>> saveStudent(@Valid @RequestBody StudentRequest studentRequest){
 
         return ResponseEntity.ok(studentService.saveStudent(studentRequest));
+
+    }
+
+    @PreAuthorize("hasAnyAuthority('Student')")
+    @PatchMapping("/updateStudent")
+    public ResponseEntity<String> updateStudent(
+            @RequestBody @Valid StudentUpdateRequestWithoutPassword studentUpdateRequestWithoutPassword,
+            HttpServletRequest httpServletRequest){
+
+        return studentService.updateStudent(studentUpdateRequestWithoutPassword, httpServletRequest);
 
     }
 
