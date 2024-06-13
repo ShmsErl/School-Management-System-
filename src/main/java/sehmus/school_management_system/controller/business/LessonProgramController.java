8f6ec8f20c3439ebe924ea893e0cab5c027c6550
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 20:08:38 +0300

    Implemented saveLessonProgram  methods in MeetingController.

--- src/main/java/sehmus/school_management_system/controller/business/LessonProgramController.java
+++ src/main/java/sehmus/school_management_system/controller/business/LessonProgramController.java
@@ -1,8 +1,15 @@
 package sehmus.school_management_system.controller.business;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.LessonProgramRequest;
+import sehmus.school_management_system.payload.responses.concretes.LessonProgramResponse;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
 import sehmus.school_management_system.services.business.LessonProgramService;
 
 @RestController
@@ -11,4 +18,13 @@ import sehmus.school_management_system.services.business.LessonProgramService;
 public class LessonProgramController {
 
     private final LessonProgramService lessonProgramService;
+
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+    @PostMapping("/save")
+    public ResponseMessage<LessonProgramResponse> saveLessonProgram(@Valid @RequestBody LessonProgramRequest lessonProgramRequest){
+
+        return lessonProgramService.saveLessonProgram(lessonProgramRequest);
+
+    }
 }
