676a54ef8a2602221d4a321b7e68b2f99ca86418
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 20:14:47 +0300

    Implemented saveEducationTerm  methods in MeetingController.

--- src/main/java/sehmus/school_management_system/controller/business/EducationTermController.java
+++ src/main/java/sehmus/school_management_system/controller/business/EducationTermController.java
@@ -1,8 +1,15 @@
 package sehmus.school_management_system.controller.business;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.EducationTermRequest;
+import sehmus.school_management_system.payload.responses.concretes.EducationTermResponse;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
 import sehmus.school_management_system.services.business.EducationTermService;
 
 @RestController
@@ -11,4 +18,13 @@ import sehmus.school_management_system.services.business.EducationTermService;
 public class EducationTermController {
 
     private final EducationTermService educationTermService ;
+
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean')")
+    @PostMapping("/save")
+    public ResponseMessage<EducationTermResponse> saveEducationTerm(@Valid @RequestBody EducationTermRequest educationTermRequest){
+
+        return educationTermService.saveEducationTerm(educationTermRequest);
+
+    }
 }
