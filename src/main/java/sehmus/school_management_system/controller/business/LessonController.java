3c24ed32a0dc46790b10c71b1430620052797bcb
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 19:45:42 +0300

    - Implemented saveLesson method in LessonController - Added @PreAuthorize annotation to restrict access to Admin, Dean, and ViceDean roles.

--- src/main/java/sehmus/school_management_system/controller/business/LessonController.java
+++ src/main/java/sehmus/school_management_system/controller/business/LessonController.java
@@ -1,4 +1,31 @@
 package sehmus.school_management_system.controller.business;
 
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.LessonRequest;
+import sehmus.school_management_system.payload.responses.concretes.LessonResponse;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.services.business.LessonService;
+
+@RestController
+@RequestMapping("/lesson")
+@RequiredArgsConstructor
 public class LessonController {
+
+
+    private final LessonService lessonService;
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+    @PostMapping("/save")
+    public ResponseMessage<LessonResponse> saveLesson(@Valid @RequestBody LessonRequest lessonRequest){
+
+        return lessonService.saveLesson(lessonRequest);
+
+    }
 }
