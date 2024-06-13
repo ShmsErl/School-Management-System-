f1029a84aa48625cdd39eb2459cb31f0eeb7c458
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 20:16:31 +0300

    Implemented getAllEducationTermsByPage  methods in MeetingController.

--- src/main/java/sehmus/school_management_system/controller/business/EducationTermController.java
+++ src/main/java/sehmus/school_management_system/controller/business/EducationTermController.java
@@ -2,12 +2,15 @@ package sehmus.school_management_system.controller.business;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import sehmus.school_management_system.payload.requests.concretes.EducationTermRequest;
 import sehmus.school_management_system.payload.responses.concretes.EducationTermResponse;
 import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
 import sehmus.school_management_system.services.business.EducationTermService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/educationTerm")
@@ -39,6 +42,19 @@ public class EducationTermController {
     public EducationTermResponse getEducationTermById(@PathVariable Long id){
 
         return educationTermService.findById(id);
+
+    }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean', 'ViceDean', 'Teacher')")
+    @GetMapping("/getAllEducationTermsByPage")
+    public Page<EducationTermResponse> getAllEducationTermsByPage(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "startDate") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type
+    ){
+
+        return educationTermService.getAllByPage(page,size,sort,type);
 
     }
 }
