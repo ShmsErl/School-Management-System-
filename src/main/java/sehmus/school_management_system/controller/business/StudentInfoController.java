c2f848505a4156731c61a473b699cf8ab65fb035
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 20:19:29 +0300

    Implemented saveStudentInfo  methods in StudentInfoController.

--- src/main/java/sehmus/school_management_system/controller/business/StudentInfoController.java
+++ src/main/java/sehmus/school_management_system/controller/business/StudentInfoController.java
@@ -1,8 +1,16 @@
 package sehmus.school_management_system.controller.business;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.StudentInfoRequest;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.payload.responses.concretes.StudentInfoResponse;
 import sehmus.school_management_system.services.business.StudentInfoService;
 
 @RestController
@@ -11,4 +19,16 @@ import sehmus.school_management_system.services.business.StudentInfoService;
 public class StudentInfoController {
 
     private final StudentInfoService studentInfoService;
+
+
+    @PreAuthorize("hasAnyAuthority('Teacher')")
+    @PostMapping("/save")
+    public ResponseMessage<StudentInfoResponse> saveStudentInfo(
+            HttpServletRequest httpServletRequest,
+            @RequestBody @Valid StudentInfoRequest studentInfoRequest){
+
+        return studentInfoService.saveStudentInfo(httpServletRequest, studentInfoRequest);
+
+    }
+
 }
