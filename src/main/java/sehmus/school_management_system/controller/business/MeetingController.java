44fcbf613bb5b3dade0c6837fe9f86a71dda44cc
ShmsErl <erol.sehmus1@gmail.com>
2024-06-13 19:56:57 +0300

    - Implemented saveMeeting  method in MeetingController.

--- src/main/java/sehmus/school_management_system/controller/business/MeetingController.java
+++ src/main/java/sehmus/school_management_system/controller/business/MeetingController.java
@@ -1,4 +1,30 @@
 package sehmus.school_management_system.controller.business;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sehmus.school_management_system.payload.requests.concretes.MeetingRequest;
+import sehmus.school_management_system.payload.responses.concretes.MeetingResponse;
+import sehmus.school_management_system.payload.responses.concretes.ResponseMessage;
+import sehmus.school_management_system.services.business.MeetingService;
+
+@RestController
+@RequestMapping("/meet")
+@RequiredArgsConstructor
 public class MeetingController {
+
+    private final MeetingService meetingService;
+
+    @PreAuthorize("hasAnyAuthority('Teacher')")
+    @PostMapping("/save")
+    public ResponseMessage<MeetingResponse> saveMeeting (HttpServletRequest httpServletRequest,
+                                                         @Valid @RequestBody MeetingRequest meetingRequest){
+
+        return meetingService.saveMeeting(httpServletRequest, meetingRequest);
+
+    }
 }
